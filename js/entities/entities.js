722335f2f28342767960145e5d1518d2c66dc657
sean goldson <seangoldson@gmail.com>
2015-01-23 12:10:43 -0800

    push 1 (1/3):comments

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -1,13 +1,19 @@
 	game.PlayerEntity = me.Entity.extend({
+		// classes have both letters capital
 		init:function(x, y, settings){
 			this._super(me.Entity, 'init', [x, y, { 
+				// this._super reaches to the constructor of entity
 				image: "player",
 				width: 64,
+				// how much space is reserved for the image
 				height: 64,
 				spritewidth: "64",
+				// passing main info size of image
 				spriteheight: "64",
 				getShape: function(){
 					return(new me.Rect(0, 0, 64, 64)).toPolygon();
+														//method:toPolygon make the Rectangle polygon,
+														// sode note all methods also fallow the naming convention that the first word is lower case and the socond is capital
 				}
 			}]);
 
