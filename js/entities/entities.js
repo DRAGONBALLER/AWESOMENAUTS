0cf85cad25982f7daef25932334de8bb9f37e3d3
sean goldson <seangoldson@gmail.com>
2015-01-29 11:55:13 -0800

    push 6 character animation

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -12,14 +12,20 @@
 				spriteheight: "64",
 				getShape: function(){
 					return(new me.Rect(0, 0, 64, 64)).toPolygon();
-														//method:toPolygon make the Rectangle polygon,
-														// sode note all methods also fallow the naming convention that the first word is lower case and the socond is capital
+				//method:toPolygon make the Rectangle polygon,
+				// sode note all methods also fallow the naming convention that the first word is lower case and the socond is capital
 				}
 			}]);
 
 			this.body.setVelocity(5,20);
 			// movement speed vurticle velocity has benn set to 20
 			// to allow the character to touch the ground
+			  
+			  this.renderable.addAnimation("idle", [78]);
+			  this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
+
+			  this.renderable.setCurrentAnimation("idle");
+			  this.renderable.setCurrentAnimation("walk");
 		},
 
 		update: function(delta) {
@@ -28,12 +34,23 @@
 				// aesVelocity() and multiplying it by me.timer.tick.
 				// me.timer.tick makes the movement look smooth
 				this.body.vel.x += this.body.accel.x * me.timer.tick;
-
+				this.flipX(true);
+	
 			}else{
 				this.body.vel.x = 0;
 			}
 
+			if(this.body.vel.x !== 0){
+			if(!this.renderable.isCurrentAnimation("walk")) {
+				this.renderable.setCurrentAnimation("walk");
+			}
+		}else{
+			this.renderable.setCurrentAnimation("idle");
+		}
+
 			this.body.update (delta);
+
+			this._super(me.Entity,"update",[delta]);
 			return true;
 		}
 	});
