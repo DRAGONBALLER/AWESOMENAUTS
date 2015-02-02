a3996e69ecb19e6538c2d923ff8b9e29c67c65fa
sean goldson <seangoldson@gmail.com>
2015-02-02 13:03:36 -0800

    final push

--- js/game.js
+++ js/game.js
@@ -40,6 +40,8 @@ var game = {
 	// Run on game resources loaded.
 	"loaded" : function () {
 		me.pool.register("player", game.PlayerEntity, true);
+		me.pool.register("PlayerBase", game.PlayerBaseEntity);
+		me.pool.register("EnimyBase", game.EnemyBaseEntity);
 		// player has been added to the "pool" of objects we can use
 		me.state.set(me.state.MENU, new game.TitleScreen());
 		me.state.set(me.state.PLAY, new game.PlayScreen());
